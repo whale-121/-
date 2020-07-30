45bb9d1404184040de472ce797b692d815789786
jeon6777 <qkqhrndud@naver.com>
2020-07-30 17:27:07 +0900

    리뷰 검색 기능 추가

--- firstProject/src/Genre_Review_Model/ReviewDAO.java
+++ firstProject/src/Genre_Review_Model/ReviewDAO.java
@@ -7,8 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
-
+import Login_view.GenreGUI;
 
 public class ReviewDAO {
 	Connection conn = null;
@@ -31,7 +30,7 @@ public class ReviewDAO {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void close() {
 
 		try {
@@ -47,17 +46,17 @@ public class ReviewDAO {
 		}
 
 	}
-	
+
 	public ArrayList<ReviewDTO> select(String Genre) {
-		
+
 		ArrayList<ReviewDTO> reviewList = new ArrayList<ReviewDTO>();
 		getConn();
 		String sql = "select * from reviews where genre = ? order by review_no asc";
 		try {
 			psmt = conn.prepareStatement(sql);
-			psmt.setString(1,Genre);
+			psmt.setString(1, Genre);
 			rs = psmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				int reviewNumber = rs.getInt(1);
 				String nickName = rs.getString(2);
 				String genre = rs.getString(3);
@@ -66,12 +65,73 @@ public class ReviewDAO {
 				String reviewName = rs.getString(6);
 				String reviewCont = rs.getString(7);
 				String reviewDate = rs.getString(8);
-				reviewList.add(new ReviewDTO(reviewNumber, nickName, genre, movieName, point, reviewName, reviewCont, reviewDate));
+				reviewList.add(new ReviewDTO(reviewNumber, nickName, genre, movieName, point, reviewName, reviewCont,
+						reviewDate));
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
+			close();
+		}
+		return reviewList;
+	}
+
+	public ArrayList<ReviewDTO> search(String a, String b) {
+		ArrayList<ReviewDTO> reviewList = new ArrayList<ReviewDTO>();
+		getConn();
+		String sql = null;
+		if (a.equals("닉네임")) {
+			sql = "select * from reviews where mem_nn = ? and genre = ? order by review_no asc";
+		} else if (a.equals("영화제목")) {
+			sql = "select * from reviews where movie_title = ? and genre = ? order by review_no asc";
+		}
+		try {
+			if (a.equals("전체검색")) {
+				sql = "select * from reviews where genre = ? and mem_nn = ? or movie_title = ? order by review_no asc";
+				psmt = conn.prepareStatement(sql);
+				psmt.setString(1, GenreGUI.genre);
+				psmt.setString(2, b);
+				psmt.setString(3, b);
+				rs = psmt.executeQuery();
+
+				while (rs.next()) {
+					int reviewNumber = rs.getInt(1);
+					String nickName = rs.getString(2);
+					String genre = rs.getString(3);
+					String movieName = rs.getString(4);
+					int point = rs.getInt(5);
+					String reviewName = rs.getString(6);
+					String reviewCont = rs.getString(7);
+					String reviewDate = rs.getString(8);
+					reviewList.add(new ReviewDTO(reviewNumber, nickName, genre, movieName, point, reviewName,
+							reviewCont, reviewDate));
+				}
+			} else {
+
+				psmt = conn.prepareStatement(sql);
+				psmt.setString(1, b);
+				psmt.setString(2, GenreGUI.genre);
+				rs = psmt.executeQuery();
+
+				while (rs.next()) {
+					int reviewNumber = rs.getInt(1);
+					String nickName = rs.getString(2);
+					String genre = rs.getString(3);
+					String movieName = rs.getString(4);
+					int point = rs.getInt(5);
+					String reviewName = rs.getString(6);
+					String reviewCont = rs.getString(7);
+					String reviewDate = rs.getString(8);
+					reviewList.add(new ReviewDTO(reviewNumber, nickName, genre, movieName, point, reviewName,
+							reviewCont, reviewDate));
+				}
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
 			close();
 		}
 		return reviewList;
